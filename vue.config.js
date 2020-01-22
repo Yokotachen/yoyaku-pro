8159bdbdd5654e2707208202db31b2bb3ffdba45
Yokota_chen <yokota_chen@aliyun.com>
2020-01-22 13:04:55 +0800

    test mac

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
     proxy: {
       "/api": {
         // search为转发路径
-        target: "http://localhost:9123", // 目标地址
+        target: "http://localhost:9123", // 目标地址 https
         ws: true, // 是否代理websockets
         changeOrigin: true // 设置同源  默认false，是否需要改变原始主机头为目标URL,
       }
